481971efcc65f969464e0fdce87ec816e15592a9
Michael Campbell <michaelcampbell@Michaels-MBP.home>
2020-07-16 23:59:53 +0100

    adds the variable onto the end of the url

--- src/Container.js
+++ src/Container.js
@@ -30,35 +30,45 @@ function Container(props){
     }
 
     function changeLinks(){
-        jsonObject['hotelBookingPage'] = "batman";
-        jsonObject['hotels'][0]['hotelBookingPage'] = "batman";
+        jsonObject['hotelBookingPage'] = jsonObject['hotelBookingPage'] + "batman";
+        jsonObject['hotels'][0]['hotelBookingPage'] = jsonObject['hotels'][0]['hotelBookingPage'] + "batman";
         jsonObject['pages'].forEach((page) => {
             if (page['type'] === "LINK_PAGE"){
-                page['url'] = "batman";
+                page['url'] = page['url'] + "batman";
             }
         })
         jsonObject['pages'].forEach((page) => {
             if(page.components){
                 page.components.forEach((component) => {
                     if(component.type === "BUTTON_COMPONENT"){
-                        if(component.buttonType === "LINK") {
-                        component['value'] = "batman";
+                        if((component.buttonType === "LINK") || (component.buttonType === "DOWNLOAD")) {
+                        component['value'] = component['value'] + "batman";
                         }
                     }
                     if(component.type === "CARD_COMPONENT"){
                         component.components.forEach((comp) => {
                             if (comp.type === "BUTTON_COMPONENT"){
-                                if(comp.buttonType === "LINK") {
-                                    comp['value'] = "batman";
+                                if((comp.buttonType === "LINK") || (comp.buttonType === "DOWNLOAD")) {
+                                    comp['value'] = comp['value'] + "batman";
                                     }
                             }
-                            if(component.type === "ACCORDION_COMPONENT"){
-                                component.components.forEach((comp) => {
-                                    if (comp.type === "BUTTON_COMPONENT"){
-                                        if(comp.buttonType === "LINK") {
-                                            comp['value'] = "batman";
+                            if(comp.type === "ACCORDION_COMPONENT"){
+                                comp.components.forEach((com) => {
+                                    if (com.type === "BUTTON_COMPONENT"){
+                                        if((com.buttonType === "LINK") || (com.buttonType === "DOWNLOAD")) {
+                                            com['value'] = com['value'] + "batman";
                                             }
                                     }
+                                    if(com.type === "ACCORDION_COMPONENT"){
+                                        com.components.forEach((banana) => {
+                                            if (banana.type === "BUTTON_COMPONENT"){
+                                                if((banana.buttonType === "LINK") || (banana.buttonType === "DOWNLOAD")) {
+                                                    banana['value'] = banana['value'] + "batman";
+                                                    }
+                                            }
+                                        })
+                                    }
+                                    
                                 })
                             }
                         })
@@ -66,15 +76,15 @@ function Container(props){
                     if(component.type === "ACCORDION_COMPONENT"){
                         component.components.forEach((comp) => {
                             if (comp.type === "BUTTON_COMPONENT"){
-                                if(comp.buttonType === "LINK") {
-                                    comp['value'] = "batman";
+                                if((comp.buttonType === "LINK") || (comp.buttonType === "DOWNLOAD")) {
+                                    comp['value'] = comp['value'] + "batman";
                                     }
                             }
                             if(comp.type === "ACCORDION_COMPONENT"){
                                 comp.components.forEach((banana) => {
                                     if (banana.type === "BUTTON_COMPONENT"){
-                                        if(banana.buttonType === "LINK") {
-                                            banana['value'] = "batman";
+                                        if((banana.buttonType === "LINK") || (banana.buttonType === "DOWNLOAD")) {
+                                            banana['value'] = banana['value'] + "batman";
                                             }
                                     }
                                 })
